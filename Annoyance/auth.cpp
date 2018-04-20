7230034d213848b942b17a155160ca847bacce16
Justin Stolle <iphreak2g@gmail.com>
2018-04-20 14:39:45 -0400

    Add files via upload

--- Annoyance/auth.cpp
+++ Annoyance/auth.cpp
@@ -1,63 +1,119 @@
 #include "stdafx.h"
 #include "hv.h"
-
-//#define UsingEncryption // remove this line if you don't want the server information to be encrypted
-#define AUTHFLAG_RESPONSE 0x0F
-#define AUTHFLAG_UPDATE 0x3F
-VOID MountPaths()
+#include "../curl/curl.h"
+#include "../json/json.h"
+#define memecpy memcpy
+#define memecmp memcmp
+char* username;
+namespace
 {
-	if ((XboxHardwareInfo->Flags & 0x20) == 0x20)
-		if (CreateSymbolicLink("HDD:\\", "\\Device\\Harddisk0\\Partition1", true) != ERROR_SUCCESS)
-			return;
-		else
-			if (CreateSymbolicLink("USB:\\", "\\Device\\Mass0", true) != ERROR_SUCCESS)
-				return;
+    std::size_t callback(
+            const char* in,
+            std::size_t size,
+            std::size_t num,
+            std::string* out)
+    {
+        const std::size_t totalBytes(size * num);
+        out->append(in, totalBytes);
+        return totalBytes;
+    }
 }
-BYTE CPUKey[16];
-BYTE *GetFile(char *szPath, PDWORD pdwOutSize)
-{
-	FILE *f = fopen(szPath, "rb");
+char* GetCpuKey(char* cpureq) {
+	BYTE step1[16];
+	BYTE step2[16];
+	BYTE step3[16];
 
-	if (!f) {
-		*pdwOutSize = 0;
+	ZeroMemory(step1, 16);
+	ZeroMemory(step2, 16);
+	ZeroMemory(step3, 16);
+
+	HRESULT success = HV::Peek::HvPeekBytes(0x20, step1, 0x10);
+	if(success != ERROR_SUCCESS)
 		return NULL;
-	}
 
-	fseek(f, 0, SEEK_END);
-	*pdwOutSize = ftell(f);
-	rewind(f);
+	BYTE part1[8];
+	memecpy(part1, (BYTE*)HV::HvGetFuseLine(3), 8);
+	BYTE part2[8];
+	memecpy(part2, (BYTE*)HV::HvGetFuseLine(5), 8);
+	memecpy(step2, part1, 8);
+	memecpy(step2 + 8, part2, 8);
 
-	BYTE *pbReturnData = (BYTE*)malloc(*pdwOutSize);
+	if(memcmp(step1, step2, 16) != 0)
+		return NULL;
 
-	fread(pbReturnData, 1, *pdwOutSize, f);
+	BYTE part3[8];
+	memecpy(part3, (BYTE*)HV::HvGetFuseLine(4), 8);
+	BYTE part4[8];
+	memecpy(part4, (BYTE*)HV::HvGetFuseLine(6), 8);
+	memecpy(step3, part3, 8);
+	memcpy(step3 + 8, part4, 8);
 
-	fclose(f);
+	if(memcmp(step1, step3, 16) != 0)
+		return NULL;
 
-	return pbReturnData;
-}
-VOID GetModuleHash(BYTE *outHash) {
-	DWORD dataSize = 0xF000;
-	BYTE *data = GetFile("HDD:\\Annoyance.xex", &dataSize);
-	
-	if (!data || dataSize == 0 || !FileExists("HDD:\\Annoyance.xex")) {
-		return;
-	}
+	if(memcmp(step2, step3, 16) != 0)
+		return NULL;
 
-	XECRYPT_SHA_STATE sha;
-	XeCryptShaInit(&sha);
-	XeCryptShaUpdate(&sha, data, dataSize);
-	XeCryptShaFinal(&sha, outHash, 20);
+	memecpy((void*)cpureq, step3, 16);
 
-	free(data);
-}
-BYTE GetCpuKey(BYTE cpureq[16]) {
-	HV::Peek::HvPeekBytes(0x20, cpureq, 0x10);
-	return cpureq[16];
+	if(memecmp((void*)cpureq, step1, 16) != 0)
+		return NULL;
+
+	std::string out = "";
+	 for (int i = 0; i < 0x10; i++, ++cpureq) {
+	  char tmp[5] = { 0 };
+	  sprintf(tmp, "%02X", (unsigned char)*cpureq);
+	  out += tmp;
+	 }
+
+	 return _strdup(out.c_str());
 }
 namespace AUTH {
 	BOOL Authenticate()
 	{
-		MountPaths(); //this needs to be fixed
+		CURL *curl;
+		CURLcode res;
+
+		curl_global_init(CURL_GLOBAL_ALL);
+
+		curl = curl_easy_init();
+		if(curl){
+			curl_easy_setopt(curl, CURLOPT_URL, "https://api.xbl.rocks/auth.php");
+			char* post;
+			sprintf(post, "auth=1&cpukey=%s", GetCpuKey(NULL));
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
+			curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+			int httpCode;
+			std::unique_ptr<std::string> httpData(new std::string());
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+			res = curl_easy_perform(curl);
+			if(res != CURLE_OK)
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+			if (httpCode == 200){
+			Json::Value jsonData;
+			Json::Reader jsonReader;
+			if(jsonReader.parse(*httpData, jsonData)){
+				bool authed(jsonData["authed"].asBool());
+				if(authed){
+					memecpy(username, jsonData["username"].asCString(), jsonData["username"].asString().length());
+					curl_easy_cleanup(curl);
+					curl_global_cleanup();
+					return true;
+				}
+				curl_global_cleanup();
+				return false;
+			}
+			curl_global_cleanup();
+			return false;
+		}
+		curl_global_cleanup();
+		return false;
+
+		/*OLD AUTH
 
 		AuthenticationRequest Request;
 		GetCpuKey(Request.CPUKey); //this needs to be fixed
@@ -134,8 +190,8 @@ namespace AUTH {
 				Sleep(5000);
 				HalReturnToFirmware(HalForceShutdownRoutine);
 			}
+		}*/
 		}
-
-		return false;
+		
 	}
 }
